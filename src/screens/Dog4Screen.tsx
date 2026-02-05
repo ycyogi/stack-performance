865f0061dc693b2360174e63b6072eed6c656aec
ycyogi <yc@ycfullstack.com>
2026-02-05 12:39:30 -0700

    chore: downgrade RN to 0.77.3 (fast-image compatibility check)

--- src/screens/Dog4Screen.tsx
+++ src/screens/Dog4Screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
-import FastImage from '@d11/react-native-fast-image';
+import FastImage from 'react-native-fast-image';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import type { FlowStackParamList } from '../navigation/types';
 
